74f273ecf1285d7b4dabf27f48fb728937f9f01a
kgarg12 <kgarg@enphaseenergy.com>
2023-03-29 14:49:13 +0530

    add model face_detect

--- face_detect.py
+++ face_detect.py
@@ -14,9 +14,17 @@ from datetime import datetime, timezone
 import pytz
 import cv2
 import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, Dense, GlobalMaxPooling2D
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.models import load_model
+
+
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
 import numpy as np
 from deepface import DeepFace
+from matplotlib import pyplot as plt
+
 from py_logging import get_logger
 import albumentations as alb
 
@@ -32,6 +40,48 @@ def return_datetime(mode=1):
     elif mode == 1:
         return dnt_pdt.strftime(date_format)
 
+
+class FaceTracker(Model):
+    def __init__(self, eyetracker, **kwargs):
+        super().__init__(**kwargs)
+        self.model = eyetracker
+
+    def compile(self, opt, classloss, localizationloss, **kwargs):
+        super().compile(**kwargs)
+        self.closs = classloss
+        self.lloss = localizationloss
+        self.opt = opt
+
+    def train_step(self, batch, **kwargs):
+        X, y = batch
+
+        with tf.GradientTape() as tape:
+            classes, coords = self.model(X, training=True)
+
+            batch_classloss = self.closs(y[0], classes)
+            batch_localizationloss = self.lloss(tf.cast(y[1], tf.float32), coords)
+
+            total_loss = batch_localizationloss + 0.5 * batch_classloss
+
+            grad = tape.gradient(total_loss, self.model.trainable_variables)
+
+        self.opt.apply_gradients(zip(grad, self.model.trainable_variables))
+
+        return {"total_loss": total_loss, "class_loss": batch_classloss, "regress_loss": batch_localizationloss}
+
+    def test_step(self, batch, **kwargs):
+        X, y = batch
+
+        classes, coords = self.model(X, training=False)
+
+        batch_classloss = self.closs(y[0], classes)
+        batch_localizationloss = self.lloss(tf.cast(y[1], tf.float32), coords)
+        total_loss = batch_localizationloss + 0.5 * batch_classloss
+
+        return {"total_loss": total_loss, "class_loss": batch_classloss, "regress_loss": batch_localizationloss}
+
+    def call(self, X, **kwargs):
+        return self.model(X, **kwargs)
 
 class FaceDetect(object):
     def __init__(self, in_folder):
@@ -54,12 +104,7 @@ class FaceDetect(object):
             logger.info("Processing video : {}".format(video))
             i_time = time.time()
             # self.face_detect_and_identify(video_path)
-            self.deepface_stream(video_path)
             logger.info("Time to process : {}".format(time.time() - i_time))
-
-    def deepface_stream(self, v_path):
-        input_db_path = "/Users/kgarg/Documents/extras/home_cam_security/input/input_db"
-        DeepFace.stream(db_path= input_db_path, enable_face_analysis=False, frame_threshold=1, time_threshold=1, detector_backend="opencv", source=v_path)
 
     def limit_gpu_growth(self):
         gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -69,14 +114,247 @@ class FaceDetect(object):
 
     def load_images(self):
         self.limit_gpu_growth()
-        data_dir_path = self.input_db_path + "kalp"
-        images = tf.data.Dataset.list_files("{}/*.jpg".format(data_dir_path), shuffle=False)
+        # data_dir_path = self.input_db_path + "kalp"
+        # images = tf.data.Dataset.list_files("{}/*.jpg".format(data_dir_path), shuffle=False)
 
         # print(images.as_numpy_iterator().next())
-        images = images.map(self.load_image)
+        # images = images.map(self.load_image)
 
         # print(images.as_numpy_iterator().next())
         # print(type(images))
+
+        # self.do_albumentation()
+        # quit()
+
+        train_images = tf.data.Dataset.list_files(os.path.join(self.input_db_path, 'aug_data', 'train', 'images', '*.jpg'), shuffle=False)
+        train_images = train_images.map(self.load_image)
+        train_images = train_images.map(lambda x: tf.image.resize(x, (500, 500)))
+        train_images = train_images.map(lambda x: x / 255)
+
+        test_images = tf.data.Dataset.list_files(os.path.join(self.input_db_path, 'aug_data', 'test', 'images', '*.jpg'), shuffle=False)
+        test_images = test_images.map(self.load_image)
+        test_images = test_images.map(lambda x: tf.image.resize(x, (500, 500)))
+        test_images = test_images.map(lambda x: x / 255)
+
+        val_images = tf.data.Dataset.list_files(os.path.join(self.input_db_path, 'aug_data', 'val', 'images', '*.jpg'), shuffle=False)
+        val_images = val_images.map(self.load_image)
+        val_images = val_images.map(lambda x: tf.image.resize(x, (500, 500)))
+        val_images = val_images.map(lambda x: x / 255)
+
+        train_labels = tf.data.Dataset.list_files(os.path.join(self.input_db_path, 'aug_data', 'train', 'labels', '*.json'), shuffle=False)
+        train_labels = train_labels.map(lambda x: tf.py_function(self.load_labels, [x], [tf.uint8, tf.float16]))
+
+        test_labels = tf.data.Dataset.list_files(os.path.join(self.input_db_path, 'aug_data', 'test', 'labels', '*.json'), shuffle=False)
+        test_labels = test_labels.map(lambda x: tf.py_function(self.load_labels, [x], [tf.uint8, tf.float16]))
+
+        val_labels = tf.data.Dataset.list_files(os.path.join(self.input_db_path, 'aug_data', 'val', 'labels', '*.json'), shuffle=False)
+        val_labels = val_labels.map(lambda x: tf.py_function(self.load_labels, [x], [tf.uint8, tf.float16]))
+
+        logger.info("len(train_images): {}, len(train_labels): {}, len(test_images): {}, len(test_labels): {}, len(val_images): {}, len(val_labels): {}".format(
+            len(train_images), len(train_labels), len(test_images), len(test_labels), len(val_images), len(val_labels)
+        ))
+
+        train = tf.data.Dataset.zip((train_images, train_labels))
+        train = train.shuffle(5000)
+        train = train.batch(8)
+        train = train.prefetch(4)
+
+        self.test1 = tf.data.Dataset.zip((test_images, test_labels))
+        self.test1 = self.test1.shuffle(1300)
+        self.test1 = self.test1.batch(8)
+        self.test1 = self.test1.prefetch(4)
+
+        val = tf.data.Dataset.zip((val_images, val_labels))
+        val = val.shuffle(1000)
+        val = val.batch(8)
+        val = val.prefetch(4)
+
+        # res = train.as_numpy_iterator().next()
+        # # res = data_samples.next()
+        #
+        # fig, ax = plt.subplots(ncols=4, figsize=(20, 20))
+        # for idx in range(4):
+        #     sample_image = res[0][idx]
+        #     sample_coords = res[1][1][idx]
+        #
+        #     cv2.rectangle(sample_image,
+        #                   tuple(np.multiply(sample_coords[:2], [120, 120]).astype(int)),
+        #                   tuple(np.multiply(sample_coords[2:], [120, 120]).astype(int)),
+        #                   (255, 0, 0), 2)
+        #
+        #     ax[idx].imshow(sample_image)
+
+        facetracker = self.build_model()
+
+        # facetracker.summary()
+        X, y = train.as_numpy_iterator().next()
+        print(X.shape)
+        classes, coords = facetracker.predict(X)
+        print(classes, coords)
+
+        batches_per_epoch = len(train)
+        lr_decay = (1. / 0.75 - 1) / batches_per_epoch
+        opt = tf.keras.optimizers.legacy.Adam(learning_rate=0.0001, decay=lr_decay)
+
+        classloss = tf.keras.losses.BinaryCrossentropy()
+        regressloss = self.localization_loss
+
+        print(self.localization_loss(y[1], coords))
+        print(classloss(y[0], classes))
+        print(regressloss(y[1], coords))
+
+        model = FaceTracker(facetracker)
+        model.compile(opt, classloss, regressloss)
+
+        logdir = 'logs'
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+        hist = model.fit(train, epochs=10, validation_data=val, callbacks=[tensorboard_callback])
+
+        print(hist.history)
+        facetracker.save('facetracker_new.h5')
+
+        fig, ax = plt.subplots(ncols=3, figsize=(20, 5))
+
+        ax[0].plot(hist.history['total_loss'], color='teal', label='loss')
+        ax[0].plot(hist.history['val_total_loss'], color='orange', label='val loss')
+        ax[0].title.set_text('Loss')
+        ax[0].legend()
+
+        ax[1].plot(hist.history['class_loss'], color='teal', label='class loss')
+        ax[1].plot(hist.history['val_class_loss'], color='orange', label='val class loss')
+        ax[1].title.set_text('Classification Loss')
+        ax[1].legend()
+
+        ax[2].plot(hist.history['regress_loss'], color='teal', label='regress loss')
+        ax[2].plot(hist.history['val_regress_loss'], color='orange', label='val regress loss')
+        ax[2].title.set_text('Regression Loss')
+        ax[2].legend()
+
+        plt.show()
+
+    def test_model(self):
+        facetracker = load_model('facetracker.h5')
+
+        for i in range(20):
+            test_data = self.test1.as_numpy_iterator()
+            test_sample = test_data.next()
+            # print(test_sample)
+            yhat = facetracker.predict(test_sample[0])
+
+            fig, ax = plt.subplots(ncols=4, figsize=(20, 20))
+            for idx in range(4):
+                sample_image = test_sample[0][idx]
+                sample_coords = yhat[1][idx]
+
+                if yhat[0][idx] > 0.9:
+                    cv2.rectangle(sample_image,
+                                  tuple(np.multiply(sample_coords[:2], [120, 120]).astype(int)),
+                                  tuple(np.multiply(sample_coords[2:], [120, 120]).astype(int)),
+                                  (255, 0, 0), 2)
+
+                ax[idx].imshow(sample_image)
+            fig.savefig("{}_{}.jpg".format(i, idx))
+
+
+    def localization_loss(self, y_true, yhat):
+        delta_coord = tf.reduce_sum(tf.square(y_true[:, :2] - yhat[:, :2]))
+
+        h_true = y_true[:, 3] - y_true[:, 1]
+        w_true = y_true[:, 2] - y_true[:, 0]
+
+        h_pred = yhat[:, 3] - yhat[:, 1]
+        w_pred = yhat[:, 2] - yhat[:, 0]
+
+        delta_size = tf.reduce_sum(tf.square(w_true - w_pred) + tf.square(h_true - h_pred))
+
+        return delta_coord + delta_size
+
+
+    def build_model(self):
+        input_layer = Input(shape=(500, 500, 3))
+
+        vgg = VGG16(include_top=False)(input_layer)
+
+        # Classification Model
+        f1 = GlobalMaxPooling2D()(vgg)
+        class1 = Dense(2048, activation='relu')(f1)
+        class2 = Dense(1, activation='sigmoid')(class1)
+
+        # Bounding box model
+        f2 = GlobalMaxPooling2D()(vgg)
+        regress1 = Dense(2048, activation='relu')(f2)
+        regress2 = Dense(4, activation='sigmoid')(regress1)
+
+        facetracker = Model(inputs=input_layer, outputs=[class2, regress2])
+        return facetracker
+
+    def load_labels(self, label_path):
+        with open(label_path.numpy(), 'r', encoding="utf-8") as f:
+            label = json.load(f)
+
+        return [label['class'], label['bbox']]
+
+    def do_albumentation(self):
+
+
+        augmentor = alb.Compose([alb.RandomCrop(width=900, height=900),
+                                 alb.HorizontalFlip(p=0.5),
+                                 alb.RandomBrightnessContrast(p=0.2),
+                                 alb.RandomGamma(p=0.2),
+                                 alb.RGBShift(p=0.2),
+                                 alb.VerticalFlip(p=0.5)],
+                                bbox_params=alb.BboxParams(format='albumentations',
+                                                           label_fields=['class_labels']))
+
+        for partition in ['train', 'test', 'val']:
+            # create aug_data dir if not exists
+            if not os.path.exists(os.path.join(self.input_db_path, 'aug_data', partition, 'images')):
+                os.makedirs(os.path.join(self.input_db_path, 'aug_data', partition, 'images'))
+
+            if not os.path.exists(os.path.join(self.input_db_path, 'aug_data', partition, 'labels')):
+                os.makedirs(os.path.join(self.input_db_path, 'aug_data', partition, 'labels'))
+
+            for image in os.listdir(os.path.join(self.input_db_path, partition, 'data')):
+                img = cv2.imread(os.path.join(self.input_db_path, partition, 'data', image))
+
+                coords = [0, 0, 0.00001, 0.00001]
+                label_path = os.path.join(self.input_db_path, partition, 'labels', f'{image.split(".")[0]}.json')
+                if os.path.exists(label_path):
+                    with open(label_path, 'r') as f:
+                        label = json.load(f)
+
+                    coords[0] = label['shapes'][0]['points'][0][0]
+                    coords[1] = label['shapes'][0]['points'][0][1]
+                    coords[2] = label['shapes'][0]['points'][1][0]
+                    coords[3] = label['shapes'][0]['points'][1][1]
+                    coords = list(np.divide(coords, [960, 1080, 960, 1080]))
+
+                try:
+                    for x in range(60):
+                        augmented = augmentor(image=img, bboxes=[coords], class_labels=['face'])
+                        cv2.imwrite(os.path.join(self.input_db_path,'aug_data', partition, 'images', f'{image.split(".")[0]}.{x}.jpg'),
+                                    augmented['image'])
+
+                        annotation = {}
+                        annotation['image'] = image
+
+                        if os.path.exists(label_path):
+                            if len(augmented['bboxes']) == 0:
+                                annotation['bbox'] = [0, 0, 0, 0]
+                                annotation['class'] = 0
+                            else:
+                                annotation['bbox'] = augmented['bboxes'][0]
+                                annotation['class'] = 1
+                        else:
+                            annotation['bbox'] = [0, 0, 0, 0]
+                            annotation['class'] = 0
+
+                        with open(os.path.join(self.input_db_path, 'aug_data', partition, 'labels', f'{image.split(".")[0]}.{x}.json'),
+                                  'w') as f:
+                            json.dump(annotation, f)
+
+                except Exception as e:
+                    print(e)
 
     def create_train_test_val_split(self):
         data_dir_path = self.input_db_path + "kalp"
@@ -95,6 +373,7 @@ class FaceDetect(object):
         if (data_type is None) | (total_data_sample == 0):
             logger.error("Total data sample can't be zero. data type is required. Can be either 'test', 'train' or 'validate'.")
             return 0
+
         #create dir if not exists
         data_type = str(data_type)
         if not os.path.exists(os.path.join(self.input_db_path, data_type, "data")):
@@ -128,20 +407,13 @@ class FaceDetect(object):
 
     def face_detect_and_identify(self, v_path):
         cap = cv2.VideoCapture(v_path)
-        face_detect_backend = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe']
         pTime = 0
         count = 1
-        segmentor = SelfiSegmentation()
-        fgbg = cv2.createBackgroundSubtractorKNN(detectShadows=False)
-        # fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
         while cap.isOpened():
             success, img = cap.read()
             if success:
                 new_size = (int(960/2), int(1080/2))
                 img = cv2.resize(img, new_size)
-                fgmask = fgbg.apply(img)
-                blur = cv2.GaussianBlur(fgmask, (9, 9), 0)
-                ret, gaus_fgmask = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
                 # gaus_fgmask = cv2.adaptiveThreshold(fgmask, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
                 # gaus_fgmask = cv2.adaptiveThreshold(fgmask, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11,
@@ -180,11 +452,6 @@ if __name__ == "__main__":
     face_detect_args.add_argument('-l', '--log_folder', type=str, metavar='face_detect_log',
                                  default="face_detect_log",
                                  help="Location of the log folder")
-    # face_detect_args.add_argument('-cn', '--camera_no', action='store', type= int, default=1, choices = range(1,5),metavar='[1-4]', help='Camera number to stream. Default is 1. Range is 1 to 4')
-    # face_detect_args.add_argument('-p', '--time_period', action='store', type=int, default=10,
-    #                              metavar='10', help='Timeperiod of saving livestream. Default is 10')
-    # face_detect_args.add_argument('-cl', '--cred_loc', action='store', metavar='cam_info.json', type=str,
-                                 # help='path of cam info file', required=True)
     face_detect_args.add_argument('-il', '--input_log_folder', type=str, metavar='input_log_folder', required=True,
                                  help="Location of the input folder which has streams")
 
@@ -196,7 +463,8 @@ if __name__ == "__main__":
     logger.info("Script version is: {}".format(face_detect_args.version))
 
     face_detect = FaceDetect(in_folder=args.input_log_folder)
-    # face_detect.video_processing_pipeline()
+    face_detect.video_processing_pipeline()
     face_detect.load_images()
-    face_detect.create_train_test_val_split()
+    # face_detect.create_train_test_val_split()
+    # face_detect.test_model()
 
