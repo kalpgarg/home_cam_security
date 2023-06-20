d40b34a52a8cf7fde1138f4001a9b40e7bf77685
kalp <kalpgarg.12@gmail.com>
2023-06-20 16:37:38 +0530

    fix fetch_streams.sh

--- sh_scripts/fetch_streams.sh
+++ sh_scripts/fetch_streams.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-REPO_DIR="$SCRIPT_DIR/.."
+REPO_DIR=$(dirname "$SCRIPT_DIR")
 echo "The repo location is: $REPO_DIR"
 
 echo "Creating virtual environment"
-source REPO_DIR/home_cam/bin/activate
+source $REPO_DIR/home_cam/bin/activate
 
 echo "Running script for fetching from camera no 1"
-output=$(python REPO_DIR/fetch_save_stream.py --cl REPO_DIR/cp_plus_cam_info.json &)
-echo "$output" > REPO_DIR/log/fetch_streams.txt
+OUTPUT_LOG=$REPO_DIR/results/fetch_streams.txt
+python $REPO_DIR/fetch_save_stream.py -cl $REPO_DIR/cp_plus_cam_info.json >> $OUTPUT_LOG 2>&1 &
 echo "fetch_save_stream is running in the background!"
-echo "Output is logged in location: $REPO_DIR/log/fetch_streams.txt"
+echo "Output is logged in location: $REPO_DIR/results/fetch_streams.txt"
 
