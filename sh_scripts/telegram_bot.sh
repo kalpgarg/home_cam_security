8952db4da02cae5f489d995a21423c7ba4ee7f73
kgarg <kalpgarg.12@gmail.com>
2024-12-09 08:50:44 +0530

    remove unneccesary print

--- sh_scripts/telegram_bot.sh
+++ sh_scripts/telegram_bot.sh
@@ -2,14 +2,14 @@
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$(dirname "$SCRIPT_DIR")
-echo "The repo location is: $REPO_DIR"
+# echo "The repo location is: $REPO_DIR"
 
-echo "Creating virtual environment"
+# echo "Creating virtual environment"
 source $REPO_DIR/home_cam/bin/activate
 
-echo "Running script for managing directory and DB"
+# echo "Running script for managing directory and DB"
 OUTPUT_LOG=$REPO_DIR/results/telegram_bot.txt
 LOG_DIR=$REPO_DIR/telegram_bot_log
 python $REPO_DIR/telegram_bot.py -cl $1 -fl $2 -msg $3 -l $LOG_DIR >> $OUTPUT_LOG 2>&1 &
 echo "telegram bot is running in the background!"
-echo "Output is logged in location: $OUTPUT_LOG"
+# echo "Output is logged in location: $OUTPUT_LOG"
